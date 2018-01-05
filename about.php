8b76d195fb1404ce32c6aaf1958631aeaf628585
Raja Kedia(miyagix) <rajakedia2222@gmail.com>
2018-01-05 14:08:47 +0530

    Added new files

---
+++ about.php
@@ -0,0 +1,13 @@
+<?php $title = "My App | About"; ?>
+
+<!doctype html>
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h2>About</h2>
+    <p>My name is raja kedia.</p>
+    <a href="index.php">Home</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php $title = "My App | Home"; ?>
+
+<!doctype html>
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h2>Hello world</h2>
+    <a href="about.php">About</a>
+</body>
+</html>
